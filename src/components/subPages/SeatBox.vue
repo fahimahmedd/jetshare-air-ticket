c62e32d92ffa122c648565beedc5c8a005d539aa
Macbook Pro Laravel <macbookprolaravel@Macbooks-MacBook-Pro.local>
2025-04-17 20:32:38 +0600

    hello

--- src/components/subPages/SeatBox.vue
+++ src/components/subPages/SeatBox.vue
@@ -1,10 +1,33 @@
 <script setup>
+const seats = reactive([
+  { id: 1, class:'seat-1', selected: false },
+  { id: 2, class:'seat-1', selected: false },
+  { id: 3, class:'seat-2', selected: false },
+  { id: 4, class:'seat-3', selected: false },
+  { id: 5, class:'seat-4', selected: false },
+  { id: 6, class:'seat-5', selected: false },
+  { id: 7, class:'seat-6', selected: false },
+]);
 </script>
 
 <template>
     <div class="seat-container" style="background-image: url('/public/images/seat/plane-plate.png')">
+
+        <v-btn
+        :class="['seat-button', { selected: seat.selected }]"
+        :style="{ top: seat.top, left: seat.left }"
+        @click="toggleSeat(seat.id)"
+        density="compact"
+        size="small"
+        color="grey-lighten-1"
+        rounded
+      >
+        {{ seat.id }}
+      </v-btn>
+
+
     </div>
-    
+
 </template>
 
 <style scoped>
